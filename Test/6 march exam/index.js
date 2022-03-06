c216988181cf6449ef2d28daf42b8f2fb9336f2e
priyankasinght <35567382+priyankasinght@users.noreply.github.com>
2022-03-06 17:29:25 +0530

    Add files via upload

---
+++ Test/6 march exam/index.js
@@ -0,0 +1,41 @@
+function Details(id) {
+    westonwear(id);
+  }
+  
+  function westonwear(id) {
+    if (id == "1") {
+    let biba = document.getElementById("id1");
+    biba.innerHTML =
+      "<h2>Blue & Grey Ethnic Motifs Printed A-Line Dress</h2><br><p>₹999</p><br><p>Blue and Grey printed woven A-line dress, has a tie-up neck, three-quarter sleeves, flared hem, an attached lining</p><br><p>Regular Fit</p><br><p>Material: Polyester</p>";
+  }
+   
+    if (id == "2") {
+        let dressbery = document.getElementById("id2");
+        dressbery.innerHTML =
+        "<h2>Women Mustard Printed Viscose Rayon Fit & Flare Dress</h2><br><p>₹1699</p><br><p>Mustard and Peach-coloured printed woven fit and flare dress, has a keyhole neck, three-quarter sleeves, button closure, and flared hem</p><br><p>Semi-Fit</p><br><p>Material: 100% Viscose Rayon</p>";
+    }
+    if (id == "3") {
+      let hm = document.getElementById("id3");
+      hm.innerHTML =
+      "<h2>Women Yellow Solid Empire Dress</h2><br><p>₹2599</p><br><p>Yellow and White solid woven empire dress, has a keyhole neck, three-quarter sleeves, button closure, an attached lining, and flounce hem</p><br><p>Semi-fit</p><br><p>Material: Cotton</p>";
+    }
+
+    if (id == "4") {
+        let forever1 = document.getElementById("id4");
+        forever1.innerHTML =
+        "<h2>Teal Green Geometric Printed Pure Cotton Fit and Flare Dress</h2><br><p>₹899</p><br><p>Semi-Fit</p><br><p>Material: 100% Cotton</p><br><p>Machine Wash</p>";
+    }
+
+    if (id == "5") {
+        let mango = document.getElementById("id5");
+        mango.innerHTML =
+        "<h2>Women Multicoloured Printed Maxi Dress</h2><br><p>₹1199</p><br><p>Semi-Fit</p><br><p>Material:Viscose Rayon</p><br><p>Dry clean</p>";
+    }
+
+    if (id == "6") {
+        let only = document.getElementById("id6");
+        only.innerHTML =
+        "<h2>Women Peach-Coloured Solid Fit and Flare Dress</h2><br><p>₹1599</p><br><p>Peach-Coloured solid woven fit and flare dress, has a round neck, long sleeves, an attached lining, and high-low hem</p><br><p></p><br><p>semi fit</p><br><p>Material:Net</p><br><p>Dry clean</p>";
+    }
+   
+  }
